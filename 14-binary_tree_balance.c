5def6d53df0f44a5801846db49ebe8d6aaf3f3cd
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-10 13:30:08 -0500

    Simplify 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,21 +29,6 @@ int recurtion(const binary_tree_t *tree)
 }
 
 /**
-* binary_tree_height - measures the height of a binary tree.
-* @tree: pointer to the root node of the tree to traverse.
-* Return: Height of a binary tree.
-*/
-
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (tree)
-	{
-		return (recurtion(tree));
-	}
-	return (0);
-}
-
-/**
  * binary_tree_balance - measures the balance factor of a binary tree.
  * @tree: pointer to the root node of the tree to measure the balance factor.
  * Return: balance of node.
@@ -55,8 +40,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		a = binary_tree_height(tree->left);
-		b = binary_tree_height(tree->right);
+		a = recurtion(tree->left);
+		b = recurtion(tree->right);
 		return (a - b);
 	}
 	return (0);
